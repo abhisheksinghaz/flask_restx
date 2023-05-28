d3e6893cb84a36fb4b55cdb2d08b938c86aad70b
Abhishek Singh <abhishek.singh@go-yubi.com>
2023-05-28 16:01:44 +0530

    edit functionality added in course & student

--- app/resources.py
+++ app/resources.py
@@ -31,10 +31,18 @@ class ViewCourses(Resource):
         return course, 201
 
 @ns.route("/courses/<int:id>")
-class ViewCourseById(Resource):
+class CourseById(Resource):
     @ns.marshal_with(course_model)
     def get(self, id):
         return Course.query.get(id)
+
+    @ns.expect(add_course_model)
+    @ns.marshal_with(course_model)
+    def put(self, id):
+        course = Course.query.get(id)
+        course.name = ns.payload.get('name')
+        db.session.commit()
+        return course, 200
 
 
 @ns.route("/students")
@@ -51,8 +59,18 @@ class ViewStudents(Resource):
         db.session.commit()
         return student, 201
 
+
 @ns.route("/students/<int:id>")
-class ViewStudentById(Resource):
+class StudentById(Resource):
     @ns.marshal_with(student_model)
     def get(self, id):
         return Student.query.get(id)
+
+    @ns.expect(add_student_model)
+    @ns.marshal_with(student_model)
+    def put(self, id):
+        student = Student.query.get(id)
+        student.name = ns.payload.get('name')
+        student.course_id = ns.payload.get('course_id')
+        db.session.commit()
+        return student, 200
