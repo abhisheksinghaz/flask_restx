c0d0bcee0d5dd51110400bf5b5af3b365aaf1efa
Abhishek Singh <abhishek.singh@go-yubi.com>
2023-05-28 14:03:43 +0530

    added routes for adding new students and courses

--- app/api_models.py
+++ app/api_models.py
@@ -1,14 +1,26 @@
 from flask_restx import fields
 from .extensions import api
 
-course_model = api.model("Course",{
-    "id": fields.Integer,
-    "name": fields.String
-})
+
 
 student_model = api.model("Student",{
     "id": fields.Integer,
     "name": fields.String,
     "course_id": fields.Integer,
-    "course": fields.Nested(course_model)
+    # "course": fields.Nested(course_model)
+})
+
+course_model = api.model("Course",{
+    "id": fields.Integer,
+    "name": fields.String,
+    # "enrolled_students": fields.Nested(student_model)
+})
+
+add_course_model  = api.model("AddCourse",{
+    "name": fields.String
+})
+
+add_student_model = api.model("AddStudent",{
+    "name": fields.String,
+    "course_id": fields.Integer
 })
